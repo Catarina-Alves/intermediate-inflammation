f4889445b4d9c85fec943b466fb89ff944fd2d7d
Catarina Alves <catarina._3@hotmail.com>
2021-12-08 12:49:23 +0000

    Add tests and new things

--- inflammation/models.py
+++ inflammation/models.py
@@ -47,19 +47,87 @@ def daily_min(data):
 
 def patient_normalise(data):
     """
-    Normalise patient data from a 2D inflammation data array.
+    Normalise patient data between 0 and 1 of a 2D inflammation data array.
 
-    NaN values are ignored, and normalised to 0.
+    Any NaN values are ignored, and normalised to 0
 
-    Negative values are rounded to 0.
+    :param data: 2d array of inflammation data
+    :type data: ndarray
+
     """
+    if not isinstance(data, np.ndarray):
+        raise TypeError('data input should be ndarray')
+    if len(data.shape) != 2:
+        raise ValueError('inflammation array should be 2-dimensional')
+    if np.any(data < 0):
+        raise ValueError('inflammation values should be non-negative')
     max = np.nanmax(data, axis=1)
     with np.errstate(invalid='ignore', divide='ignore'):
         normalised = data / max[:, np.newaxis]
     normalised[np.isnan(normalised)] = 0
-    normalised[normalised < 0] = 0
     return normalised
 
-# TODO(lesson-design) Add Patient class
+
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(Person):
+    """A patient in an inflammation study."""
+    def __init__(self, name, observations=None):
+        super().__init__(name)
+
+        self.observations = []
+        if observations is not None:
+            self.observations = observations
+
+    def add_observation(self, value, day=None):
+        if day is None:
+            try:
+                day = self.observations[-1].day + 1
+
+            except IndexError:
+                day = 0
+
+        new_observation = Observation(value, day)
+
+        self.observations.append(new_observation)
+        return new_observation
+
+
+class Doctor(Person):
+    """A doctor in an inflammation study."""
+    def __init__(self, name):
+        super().__init__(name)
+        self.patients = []
+
+    def add_patient(self, patient_name):
+        new_patient = Patient(patient_name)
+
+        self.patients.append(new_patient)
+        return new_patient
+
+    @property
+    def patient_names(self):
+        patients = self.patients
+
+        patient_names_list = []
+        for patient in patients:
+            patient_names_list.append(patient.name)
+        return patient_names_list
+
+
 # TODO(lesson-design) Implement data persistence
-# TODO(lesson-design) Add Doctor class
--- tests/test_patient.py
+++ tests/test_patient.py
@@ -8,3 +8,45 @@ def test_create_patient():
     p = Patient(name=name)
 
     assert p.name == name
+
+
+def test_create_doctor():
+    from inflammation.models import Doctor
+
+    name = 'Alice'
+    doctor = Doctor(name=name)
+
+    assert doctor.name == name
+
+
+def test_add_patients():
+    from inflammation.models import Doctor
+
+    # Names of doctor and patients
+    doctor_name = 'Alice'
+    patient_1_name = 'Bob'
+    patient_2_name = 'Carl'
+
+    # Initialise doctor class and add patients
+    doctor = Doctor(name=doctor_name)
+    doctor.add_patient(patient_name=patient_1_name)
+    doctor.add_patient(patient_name=patient_2_name)
+
+    assert doctor.patients[0].name == 'Bob'
+    assert doctor.patients[1].name == 'Carl'
+
+
+def test_patient_names():
+    from inflammation.models import Doctor
+
+    # Names of doctor and patients
+    doctor_name = 'Alice'
+    patient_1_name = 'Bob'
+    patient_2_name = 'Carl'
+
+    # Initialise doctor class and add patients
+    doctor = Doctor(name=doctor_name)
+    doctor.add_patient(patient_name=patient_1_name)
+    doctor.add_patient(patient_name=patient_2_name)
+
+    assert doctor.patient_names == ['Bob', 'Carl']
